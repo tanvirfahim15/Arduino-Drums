f30358a7fc8de03915dd2990204d1272ee6d51fa
Tanvir Fahim <tanvircsedu15@gmail.com>
2017-11-08 01:40:18 +0600

    Add files via upload

---
+++ sketch_oct24a/sketch_oct24a.ino
@@ -0,0 +1,20 @@
+int analogPin0=0,analogPin1=1;
+
+void setup()
+{
+  Serial.begin(9600);              //  setup serial
+}
+
+void loop()
+{
+  int val = analogRead(analogPin0);
+  if(val>20){
+    Serial.println(val);
+    delay(500);
+  }
+  val = analogRead(analogPin1);
+  if(val>20){
+    Serial.println(-val);
+    delay(100);
+  }
+}
