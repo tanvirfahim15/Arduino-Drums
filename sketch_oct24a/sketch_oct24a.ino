eb488c8a538fc0ed934f85d39cde4f3b1fa2a6d7
Tanvir Fahim <tanvircsedu15@gmail.com>
2017-11-08 19:42:31 +0600

    Update sketch_oct24a.ino

--- sketch_oct24a/sketch_oct24a.ino
+++ sketch_oct24a/sketch_oct24a.ino
@@ -1,12 +1,43 @@
 int analogPin0=0,analogPin1=1;
+ 
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+ 
+const int buzzerPin = 13;
+const int ledPin1 = 11;
+const int ledPin2 = 12;
+ 
+int counter = 0;
 
 void setup()
 {
   Serial.begin(9600);              //  setup serial
+  
+  
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  
+  play();
 }
 
 void loop()
 {
+  
   int val = analogRead(analogPin0);
   if(val>20){
     Serial.println(val);
@@ -17,4 +48,85 @@ void loop()
     Serial.println(-val);
     delay(100);
   }
+}
+void play(){
+  //Play first section
+  firstSection();
+ 
+  //Play second section
+  secondSection();
+}
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note, duration);
+ 
+  //Play different LED depending on value of 'counter'
+  if(counter % 2 == 0)
+  {
+    digitalWrite(ledPin1, HIGH);
+    delay(duration);
+    digitalWrite(ledPin1, LOW);
+  }else
+  {
+    digitalWrite(ledPin2, HIGH);
+    delay(duration);
+    digitalWrite(ledPin2, LOW);
+  }
+ 
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+ 
+  delay(50);
+ 
+  //Increment counter
+  counter++;
+}
+ 
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+ 
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+}
+ 
+void secondSection()
+{
+  beep(a, 300);
+  beep(a, 150);
+  beep(fSH, 125);
+  beep(fH, 125);    
+  beep(fSH, 250);
+ 
+  delay(325);
+ 
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);  
+  beep(cSH, 175);  
+  beep(cH, 125);  
+  beep(b, 125);  
+  beep(cH, 250);  
+ 
+  delay(350);
 }
